5b72255fa7a7eaaf51a599fc35502c5f97457921
Josue Gomez <jgomez@jesrat.com>
2019-05-14 19:00:16 -0600

    rename pkg

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyutils",
+    name="pyutl",
     version="2.1",
     author="Josue Gomez",
     author_email="jgomez@binkfe.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://git.binkfe.com/jesrat/pyutils",
-    packages=['pyutils'],
+    packages=['pyutl'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
