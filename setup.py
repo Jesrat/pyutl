637b9a245d1fa9ba6a0ad0da688dd1d421d5b511
Josue Gomez <jgomez@jesrat.com>
2019-05-14 18:33:38 -0600

    make it package2

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyutils",
+    version="2.0",
+    author="Josue Gomez",
+    author_email="jgomez@binkfe.com",
+    description="A package of utilities for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.binkfe.com/jesrat/pyutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
